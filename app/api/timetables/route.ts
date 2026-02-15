711f0a654e0d5660089185efe0ff16860458550c
ishimweghislain <ishimweghislain82@gmail.com>
2026-02-16 10:01:09 +1300

    oke

--- app/api/timetables/route.ts
+++ app/api/timetables/route.ts
@@ -32,6 +32,7 @@ export async function GET(request: Request) {
                 term: true,
                 academicYear: true,
             },
+     
             orderBy: [
                 { day: 'asc' },
                 { startTime: 'asc' }
